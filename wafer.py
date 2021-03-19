fdc9240545e038a78ecec887882f88ceb85e2a68
jjm0108 <jjm0108@nepes.co.kr>
2021-03-19 11:29:01 +0900

    processing result added version

--- wafer.py
+++ wafer.py
@@ -15,28 +15,33 @@ STEP = 0
 START = None
 END = None
 
-param_l = consts.numeric
+param_l = consts.chamber
 
 info = {}
-columns = ['start','end']
+columns = ['start','end','step','recipe']
 for parameter in param_l : columns.append(parameter)
 DATA=pd.DataFrame(columns=columns)
 
 
 for index, row in ch.iterrows():
     #step이 달라졌을 때
+    if index % 10000 == 0: print('processing ...', index)
     if STEP != row.step:
-        if index <1 : info['end'] = row.time
-        else: info['end'] = ch.loc[index-1].time
+        if index <1 : 
+            info['end'] = row.time
+        else: 
+            info['end'] = ch.loc[index-1].time
         #이전 결과 출력
         for parameter in param_l : 
             if parameter in info.keys() : info[parameter] = np.mean(info[parameter])
-        print(info)
-        if len(info) >1 : DATA = DATA.append(info, ignore_index=True)
-        print(DATA)
+        #print(info)
+        if len(info)>=len(param_l) : DATA = DATA.append(info, ignore_index=True)
+        #print(DATA)
         
         #new
         info['start'] = row.time
+        info['recipe'] = row.recipe
+        info['step'] = row.step
         for parameter in param_l : 
             info[parameter] = [row[parameter]]
         STEP = row.step
@@ -48,4 +53,4 @@ for index, row in ch.iterrows():
         
         
                 
-
+DATA.to_csv('./summary.csv', sep=',')
